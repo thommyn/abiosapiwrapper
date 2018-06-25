caa9d3309dae6a2ef4c42e4125f418cf0edf9993
thommyn <thommy.norman@gmail.com>
2018-06-25 23:38:13 +0200

    JQuery from jquery string

--- jsonconv/factory.go
+++ jsonconv/factory.go
@@ -1,32 +1,17 @@
 package jsonconv
 
-import (
-	"fmt"
-)
+const PathNodeSeparator = "/"
 
-type JsonConverter interface {
-	Convert(injson []interface{}) (outjson []interface{}, err error)
+type JsonQueryFactory interface {
+	Get(convType string) JsonQuery
 }
 
-type JsonConverterFactory interface {
-	Get(convType string) (JsonConverter, error)
+type defaultJsonQueryFactory struct {}
+
+func NewDefaultJsonConverterFactory() JsonQueryFactory {
+	return &defaultJsonQueryFactory{}
 }
 
-type defaultJsonConverterFactory struct {}
-
-func NewDefaultJsonConverterFactory() JsonConverterFactory {
-	return &defaultJsonConverterFactory{}
-}
-
-func (f defaultJsonConverterFactory) Get(convType string) (JsonConverter, error) {
-	switch convType {
-	case "none":
-		return nil, nil
-	case "liveplayers":
-		return NewPlayersFromSeries(), nil
-	case "liveteams":
-		return NewTeamsFromSeries(), nil
-	default:
-		return nil, fmt.Errorf("unknown converter type %s", convType)
-	}
+func (f defaultJsonQueryFactory) Get(path string) JsonQuery {
+	return NewJsonQuery(path, PathNodeSeparator)
 }
