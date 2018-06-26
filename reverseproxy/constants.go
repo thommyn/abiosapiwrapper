f87f0b74d162be93f4295dfc39b9845019057c30
thommyn <thommy.norman@gmail.com>
2018-06-27 01:32:28 +0200

    Added constants used in reverseproxy package

---
+++ reverseproxy/constants.go
@@ -0,0 +1,8 @@
+package reverseproxy
+
+const HttpSuccessOk int = 200
+const HttpClientErrorForbidden = 403
+const HttpHeaderContentEncoding string = "Content-Encoding"
+const HttpHeaderContentLength string = "Content-Length"
+const HttpHeaderContentEncodingType string = "gzip"
+const JsonDataNodeName string = "data"
