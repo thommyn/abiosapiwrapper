e95525b433504f533f8887d02a7298580415acd9
thommyn <thommy.norman@gmail.com>
2018-06-27 01:22:00 +0200

    Formatting code

--- reverseproxy/director_test.go
+++ reverseproxy/director_test.go
@@ -10,18 +10,18 @@ func Test_DirectorFunc_NotAllowedArgs_ReturnsError(t *testing.T) {
 	var expectedRawQuery string = "q1=1&q2=2&q3=3&q4=4"
 
 	target := &url.URL{
-		Host: "api.target.com",
-		Scheme: "https",
-		Path: "/apitarget/testtarget",
+		Host:     "api.target.com",
+		Scheme:   "https",
+		Path:     "/apitarget/testtarget",
 		RawQuery: "q3=3&q4=4",
 	}
 
 	request := &http.Request{
 		Host: "",
-		URL: &url.URL {
-			Host: "api.request.com",
-			Scheme: "https",
-			Path: "/apireq/testreq",
+		URL: &url.URL{
+			Host:     "api.request.com",
+			Scheme:   "https",
+			Path:     "/apireq/testreq",
 			RawQuery: "q1=1&q2=2",
 		},
 	}
